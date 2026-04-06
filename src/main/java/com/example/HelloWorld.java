ed733ac0d6dabb0dce5ee3a5449d4565fb50aa12
yj <lowyongjin0227@gmailcom>
2026-04-06 15:49:48 +0800

    second commit

--- src/main/java/com/example/HelloWorld.java
+++ src/main/java/com/example/HelloWorld.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); //comment
+        System.out.println("Hello, Student!"); //comment
     }
 
     public String getGreeting() {
-        return "Hello, World!";
+        return "Hello, Student!";
     }
 }
